ff77d419a964d04a08c2acf9359e09b8a0b45ca9
ViktorJJF <vj.jimenez96@gmail.com>
2024-10-07 21:03:51 -0500

    Update: changed backend url

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const IS_DEV: boolean = window.location.hostname === 'localhost';
 
 const BACKEND_BASE_URL: string = IS_DEV
   ? 'http://localhost:3000'
-  : 'https://underbot2.herokuapp.com';
+  : 'https://underbot3-1c596068abaf.herokuapp.com';
 
 const DASHBOARD_BASE_URL: string = IS_DEV
   ? 'http://localhost:3000'
